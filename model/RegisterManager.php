2c77fc3e7a47cd32f49bb25426da57095225696a
Bamboo <a.bedoya@outlook.fr>
2019-01-31 16:09:39 +0100

    Mise à jour model backend
    Fonction ajout et récupérer admin pour le blog

--- model/RegisterManager.php
+++ model/RegisterManager.php
@@ -6,22 +6,19 @@ require_once("model/Manager.php");
 
 class RegisterManager extends Manager
 {
-	public function addAdmin($pseudo, $pass)
+	public function addAdmin($pseudo, $email, $pass_hashe)
 	{
-		$db = $this->dbConnect();
-		$inscription = $db->prepare("INSERT INTO membres (pseudo, email, pass, date_inscription) VALUES('$pseudo', '$email', '$password', now()");
-		$dataAdmin = $inscription->execute(array($pseudo, $pass);
-			
-		return $dataAdmin;
+        $db = $this->dbConnect();     
+		$inscription = $db->prepare('INSERT INTO membres (pseudo, email, pass, date_inscription) VALUES(:pseudo, :email, :pass, now())');
+		$dataAdmin = $inscription->execute(array('pseudo' => $pseudo, 'email' => $email , 'pass' => $pass_hashe));
 	}
+    public function getAdmin($pseudo)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, pass FROM membres WHERE pseudo = :pseudo');   
+        $req->execute(array('pseudo' => $pseudo));
+        $result = $req->fetch();
 
-	public function getAdmin($id)
-	{
-		$db = $this->dbconnect();
-		$req = $db->prepare('SELECT pseudo, pass FROM membres WHERE id = ?');
-		$req->execute(array($id));
-        $gadmin = $req->fetch();
-
-        return $gadmin;
-	}
+        return $result;
+    }
 }
