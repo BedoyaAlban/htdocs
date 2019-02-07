acff418508174ce3ae9ecca6fe0ad4271bde986b
Bamboo <a.bedoya@outlook.fr>
2019-02-07 13:34:20 +0100

    MIse à jour
    fonction vérifier pseudo + utilisation de la fonction trim

--- controller/backend.php
+++ controller/backend.php
@@ -6,13 +6,32 @@ require_once('model/RegisterManager.php');
 function addAdmin($pseudo, $email, $pass_hashe) 
 {
 	$registerManager = new \Tp\Blog\Model\RegisterManager();
+    
+    $pseudoV = trim($pseudo);
+    
+    $found = $registerManager->verifyPseudo($pseudo);
+  
+    if ($found != 0) {
+    	throw new Exception('Pseudo déjà utilisé!');
 
-	$pass_hashe = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+    }
+	if ($_POST['password_1'] != $_POST['password_2']) {
+    	throw new Exception('Les deux mots de passe ne correspondent pas!');
+    }
+	if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email']))
+    {
+        $pass_hashe = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+    
+		$dataAdmin = $registerManager->addAdmin($pseudo, $email, $pass_hashe);
 
-	$dataAdmin = $registerManager->addAdmin($pseudo, $email, $pass_hashe);
-
-	require('view/backend/connexionView.php');
+		require('view/backend/connexionView.php');
+    } else
+    {
+        throw new Exception('L\'adresse ' . $_POST['email'] . ' n\'est pas valide, recommencez !');
+	   
+	}
 }
+
 function getAdmin($pseudo)
 {
 	$registerManager = new \TP\Blog\Model\RegisterManager();
@@ -27,14 +46,21 @@ function getAdmin($pseudo)
 	{
 	    if ($isPasswordCorrect) {
 	        session_start();
-	        $_SESSION['id'] = $resultat['id'];
+	        $posts = $registerManager->getPostsAdmin();
+	        $_SESSION['id'] = $result['id'];
+	        var_dump($_SESSION['id']);
+	        die();
 	        $_SESSION['pseudo'] = $pseudo;
 	        echo 'Vous êtes connecté !';
 
 	        require('view/backend/adminView.php');
 	    }
 	    else {
-	        throw new Exception('Mauvais identifiant ou mot de passe !');
+	        throw new Exception('Mauvais identifiant ou mot de passe !'); 
+	         /* ToDo : - message à changer 
+	         - changer la vue admin créer un template à la place
+	         - si problème fctn addadmin renvoyer à la vue de connexion 
+	         - s'inspirer de wordpress pour la fonctionnalité admin  créer les mockups des vues admin avant le reste */
 	    }
 	}
 }
