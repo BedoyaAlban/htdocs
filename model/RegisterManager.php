a4d33243c3c4968f91852b40af8f94d16a5a7e7f
Bamboo <a.bedoya@outlook.fr>
2019-02-21 10:54:26 +0100

    Mise à jour model backend
    Nouvelles fonctions récupérer éditer supprimer

--- model/RegisterManager.php
+++ model/RegisterManager.php
@@ -12,6 +12,7 @@ class RegisterManager extends Manager
 		$inscription = $db->prepare('INSERT INTO membres (pseudo, email, pass, date_inscription) VALUES(:pseudo, :email, :pass, now())');
 		$dataAdmin = $inscription->execute(array('pseudo' => $pseudo, 'email' => $email , 'pass' => $pass_hashe));
 	}
+
     public function getAdmin($pseudo)
     {
         $db = $this->dbConnect();
@@ -21,10 +22,11 @@ class RegisterManager extends Manager
 
         return $result;
     }
+
     public function getPostsAdmin()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date LIMIT 0, 5');
 
         return $req;
     }
@@ -37,5 +39,36 @@ class RegisterManager extends Manager
         $found = $req->fetch();
 
         return $found;
+    }
+
+    public function getPostAdmin($postId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req->execute(array($postId));
+        $post = $req->fetch();
+
+        return $post;
+    }
+
+    public function addNewPost($title, $content)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('INSERT INTO posts (title, content, creation_date) VALUES(?, ?, now())');
+        $newPost = $req->execute(array($title, $content));
+    }
+
+    public function deletePost($id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('DELETE FROM posts WHERE id = ?');
+    }
+
+    public function editPost($title, $content, $id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE posts SET title = ?, content = ? WHERE id = ?');
+        $postEdit = $req->execute(array($title, $content, $id));
+
     }
 }
